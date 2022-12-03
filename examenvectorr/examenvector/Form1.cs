eea66da07c3340941f5ff71234232f2fe1222f60
jcepro1 <115606134+jcepro1@users.noreply.github.com>
2022-12-02 20:47:12 -0400

    Add files via upload

---
+++ examenvectorr/examenvector/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace examenvector
+{
+    public partial class Form1 : Form
+    {
+        Vector v1,v2,v3;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            v1 = new Vector();
+            v2 = new Vector();
+            v3 = new Vector();
+
+          
+        }
+
+        private void cargarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int n1, a, b;
+
+            n1 = Int32.Parse(textBox1.Text);
+            a = Int32.Parse(textBox2.Text);
+            b = Int32.Parse(textBox3.Text);
+
+            v1.CargarRamdom(n1, a, b);
+        }
+
+        private void descargarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = v1.Descargar();
+        }
+
+        private void pregunta1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            v1.examen1(v2);
+            textBox6.Text = v2.Descargar();
+        }
+
+        private void pregunta2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int a, b;
+            a = Int32.Parse(textBox2.Text);
+            b = Int32.Parse(textBox3.Text);
+            v1.examen2(a, b);
+            textBox5.Text = v1.Descargar();
+        }
+
+        private void cargarToolStripMenuItem1_Click_1(object sender, EventArgs e)
+        {
+            int n1, a, b;
+
+            n1 = Int32.Parse(textBox1.Text);
+            a = Int32.Parse(textBox2.Text);
+            b = Int32.Parse(textBox3.Text);
+
+            v2.CargarRamdom(n1, a, b);
+        }
+
+        private void descargarToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            textBox5.Text = v2.Descargar();
+        }
+
+        private void modeloExamen2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            
+
+        }
+    }
+}
---
+++ examenvectorr/examenvector/Nent.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace examenvector
+{
+    class Nent
+    {
+        private int n;
+        public Nent()
+        {
+            n = 0;
+        }
+        public void cargar(int dato)
+        {
+            n = dato;
+        }
+
+        public void RamdomCARGAR(int a, int b)
+        {
+            Random r;
+            r = new Random();
+
+            n = r.Next(a, b);
+        }
+
+        public int descargar()
+        {
+            return n;
+        }
+        public bool veriffibo()
+        {
+            int a, b, c;
+            a = -1;
+            b = 1;
+            c = 0;
+            while (c < n)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            if (c == n)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
