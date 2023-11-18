505a86355a836c9b6a32dd2b63dc0448e6ee46c8
jcepro1 <115606134+jcepro1@users.noreply.github.com>
2023-11-18 12:50:42 -0400

    Add files via upload

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Vector v1, v2, v3;
+        Nent n1, n2, n3;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            v1 = new Vector();
+            v2 = new Vector();
+            v3 = new Vector();
+            n1 = new Nent();
+            n2 = new Nent();
+            n3 = new Nent();
+        }
+
+
+        //CARGAR 1
+
+        private void cARGARToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            v1.cargardato(int.Parse(textBox1.Text));
+            textBox5.Text = string.Concat(v1.descargar());
+        }
+
+        private void cARGARRNDToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            v1.Cargar_rnd(int.Parse(textBox1.Text), int.Parse(textBox2.Text), int.Parse(textBox3.Text));
+            textBox5.Text = string.Concat(v1.descargar());
+        }
+
+        private void dESCARGARToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox6.Text = string.Concat(v1.descargar());
+        }
+
+        //CARGAR 2
+        private void cARGARToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            //v2.cargardato(int.Parse(textBox1.Text));
+            //textBox5.Text = string.Concat(v2.descargar());
+        }
+
+        private void cARGARRBDToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //v2.Cargar_rnd(int.Parse(textBox1.Text), int.Parse(textBox2.Text), int.Parse(textBox3.Text));
+            //textBox5.Text = string.Concat(v2.descargar());
+        }
+
+
+        private void dESCARGARToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            //textBox7.Text = string.Concat(v2.descargar());
+        }
+
+
+        //CARGAR 3
+        private void cARGARToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            //v3.cargardato(int.Parse(textBox1.Text));
+            //textBox5.Text = string.Concat(v3.descargar());
+        }
+
+        private void cARGARRNDToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            //v3.Cargar_rnd(int.Parse(textBox1.Text), int.Parse(textBox2.Text), int.Parse(textBox3.Text));
+            //textBox5.Text = string.Concat(v3.descargar());
+        }
+
+        private void dESCARGARToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            //textBox8.Text = string.Concat(v3.descargar());
+        }
+
+
+
+        private void pREGUNTA1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int a, b;
+            a = Int32.Parse(textBox2.Text);
+            b = Int32.Parse(textBox3.Text);
+            v1.ejercicio1(a, b);
+            textBox7.Text = v1.descargar();
+        }
+
+        private void pREGUNTA2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+          //  textBox9.Text = String.Concat(v1.Elem_MenosRep_Seg_frec(int.Parse(textBox2.Text), int.Parse(textBox3.Text)));
+            textBox8.Text = String.Concat(v1.Elem_MenosRep_Seg(int.Parse(textBox2.Text), int.Parse(textBox3.Text)));
+
+        }
+
+
+    }
+}
---
+++ WindowsFormsApp1/Vector.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace WindowsFormsApp1
+{
+    class Vector
+    {
+        const int MAX = 100;
+        private int[] v;
+        private int n;
+        public Vector()
+        {
+            n = 0;
+            v = new int[MAX];
+        }
+
+        public void cargardato(int nele)
+        {
+            n = nele;//JCE
+            int num = n;
+            for (int i = 1; i <= num; i++)
+            {//JCE
+                v[i] = Conversions.ToInteger(Interaction.InputBox(" ", " "));
+            }
+        }
+        public void Cargar_rnd(int n1, int a, int b)
+        {
+            Random r;
+            r = new Random();
+            int i;
+            n = n1;
+            for (i = 1; i <= n; i++)
+            {
+                v[i] = r.Next(a, b);
+            }
+
+        }
+
+        public string descargar()
+        {
+            string s;
+            int i;
+            s = "";
+            for (i = 1; i <= n; i++)
+            {
+                s = s + v[i] + " | ";
+            }
+            return s;
+        }
+
+     
+
+        //EJERCICIO 1
+        public void ejercicio1(int a, int b)
+        {
+            Vector vr = new Vector();
+            for (int i = a; i <= b; i++)
+            {
+                vr.insertar(v[i]);
+            }
+            vr.ordenar();
+            int x = 1;
+            int y = vr.n;
+            bool bandera = true;
+            for (int i = a; i <= b; i++)
+            {
+                if (bandera)
+                {
+                    v[i] = vr.v[y];
+                    y--;
+                }
+                else
+                {
+                    v[i] = vr.v[x];
+                    x++;
+                }
+                bandera = !bandera;
+            }
+
+        }
+        private void insertar(int ele)
+        {
+            n++;
+            v[n] = ele;
+        }
+
+        public void ordenar()
+        {
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = i + 1; j <= n; j++)
+                {
+                    if (v[i] > v[j])
+                    {
+                        int aux = v[i];
+                        v[i] = v[j];
+                        v[j] = aux;
+                    }
+                }
+            }
+        }
+
+        //EJERCICIO 2
+        public string Elem_MenosRep_Seg(int a, int b)
+        {
+            int i = a;
+            int fr = frec_elem_segmento(v[i], a, b);
+            int ele = v[i];
+            for (i = a; i <= b; i++)
+            {
+                if (frec_elem_segmento(v[i], a, b) > fr)
+                {
+                    fr = frec_elem_segmento(v[i], a, b);
+                    ele = v[i];
+                    
+                }
+            }
+            return "ele: "+ele+" , frac: "+fr;
+        }
+
+        public int Elem_MenosRep_Seg_frec(int a, int b)
+        {
+            int i = a;
+            int frexMen;
+            int c;
+            c = 0;
+            int fr = frec_elem_segmento(v[i], a, b);
+            int ele = v[i];
+            for (i = a; i <= b; i++)
+            {
+                frexMen = frec_elem_segmento(v[i], a, b);
+                if (frexMen > fr)
+                {
+                    fr = frexMen;
+                  
+                    c = c + 1;
+                }
+            }
+            return fr;
+        }
+
+
+
+        public int frec_elem_segmento(int elem, int a, int b)
+        {
+            int c = 0;
+            int num = a;
+            int num2 = b;
+            checked
+            {
+                for (int i = num; i <= num2; i++)
+                {
+
+                    if (elem == v[i])
+                    {
+                        c++;
+                    }
+                }
+                return c;
+            }
+        }
+
+
+
+    }
+}
